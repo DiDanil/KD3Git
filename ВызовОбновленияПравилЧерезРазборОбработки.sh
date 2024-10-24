57ea592fe72537379449f6faa3d1c7d462b55ed4
DanilD <158293193+DanilDTest@users.noreply.github.com>
2024-10-24 20:15:51 +0300

    Параметризация
    Параметризация

---
+++ ВызовОбновленияПравилЧерезРазборОбработки.sh
@@ -0,0 +1,30 @@
+set +o history
+
+gitRepoName=$(basename `git rev-parse --show-toplevel`)
+RulesCatPath="/d/Общая/Кд3Обмены/Срань" ##Где ищем обработку с правилами
+
+gitBranchName="main"
+commit="Обновление правил $gitRepoName"
+
+gitHome="/d/Общая/git/rep" ##Кореновой каталог репозиториев / Вынести в общие настройки
+gitKD3GitPath="$gitHome/KD3Git" ##Вынести в общие настройки
+
+cd "$RulesCatPath"
+
+echo "Обработка для разбора"
+select fname in $(find . -type f  -name "*.epf" -print0 | xargs -0 -exec ls -t| head -n 10);
+do
+	if [ -n "$fname" ]; then
+		EPFPath="$RulesCatPath/$fname"
+		break;
+	else
+		echo "Неверный выбор. Пожалуйста, попробуйте снова."
+    fi
+done
+
+read -e -p 'branch: ' -i "$gitBranchName" gitBranchName
+read -e -p 'Текст коммита(номер запроса): ' -i "$commit" commit
+
+"$gitKD3GitPath/ОбновитьПравилаРазобравОбработку.sh" "$gitRepoName" "$gitBranchName" "$commit"  "$EPFPath"
+
+$SHELL
---
+++ ОбновитьПравилаРазобравОбработку.sh
@@ -0,0 +1,56 @@
+
+gitRepoName=$1 ##Имя репозитория
+gitBranchName=$2
+commit=$3 ##Комментарий коммита
+EPFPath=$4 ##Обработка менеджера
+
+
+
+
+gitHome="/d/Общая/git/rep" ##Кореновой каталог репозиториев / Вынести в общие настройки
+gitKD3GitPath="$gitHome/KD3Git" ##Вынести в общие настройки
+gitIgnoreDirName="Ignore" ##Вынести в общие настройки/Чет херня какая то
+
+gitCatPath="$gitHome/$gitRepoName" ##Каталог репозитория правил
+gitRulesPath="$gitCatPath/ПравилаОбмена" ##Правила обмена разобранные на функции
+ResDisassemblyCat="$gitCatPath/Ignore/РезультатРазбораОбработкиНаИсходники" ##Обработка менеджера разобранная на исходники
+
+
+RulesFileName="МенеджерОбмена.txt" ##Текстовый файл с менеджером обмена, получается из разбора обработки исходники, передается в разбор на процедуры
+
+
+##cd "$RulesCatPath"
+
+mkdir "$gitCatPath/$gitIgnoreDirName" 2>/dev/null
+rm -r "$ResDisassemblyCat" 2>/dev/null
+mkdir "$ResDisassemblyCat" 2>/dev/null
+
+#В ИФ через И докинуть cp
+#Мб имеет смысл выгружать результат разбора в отдельный подкаталог чтоб они перезатирались?
+
+##																Куда сохранить Что разбирать
+if "$gitKD3GitPath/РазборОбработкиНаИсходникиЧерезКонфигуратор.sh" "$ResDisassemblyCat" "$EPFPath" "$(pwd)\out.txt"; then
+	
+	cd "$gitCatPath"
+	git pull ##???
+	
+	cd "$ResDisassemblyCat"
+	cp $(find "$ResDisassemblyCat" -name '*.bsl') "$RulesFileName";
+
+	oscript "$gitKD3GitPath/РазборПравилОбмена.os" "$ResDisassemblyCat/$RulesFileName" "$gitCatPath/ПравилаОбмена"
+
+	cd "$gitCatPath"
+	
+	if echo $(git branch)|grep -q "$gitBranchName"; then
+		git switch "$gitBranchName"
+	else
+		git switch -c "$gitBranchName"
+	fi
+
+	git add . 
+	git commit -m "$commit"
+else 
+	echo $?
+fi
+
+$SHELL
